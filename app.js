87c7a4e800055cb4e120bee50389c501a4d78e63
Omkar Jagtap <omkarjagtap1011@gmail.com>
2024-05-24 08:13:34 +0530

    Implemented CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
@@ -20,10 +21,18 @@ const viewRouter = require('./routes/viewRoutes');
 // Start Express App
 const app = express();
 
+app.enable('trust-proxy');
+
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1) GLOBAL MIDDLEWARES
+
+// Implementing CORS
+app.use(cors());
+// Access-Control-Allowed-Origin : *
+
+app.options('*', cors());
 
 // Serving Static Files
 app.use(express.static(path.join(__dirname, 'public')));
