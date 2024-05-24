16c3a3f9b0d936d39022d382de03865bd5ac90cc
Omkar Jagtap <omkarjagtap1011@gmail.com>
2024-05-24 16:03:31 +0530

    Solved All Errors and Ready for deployment

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -2,7 +2,6 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Tour = require('../models/tourModel');
 const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
-const AppError = require('../utils/appError');
 const factory = require('./handlerFactory');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
