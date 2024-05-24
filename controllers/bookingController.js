a96cc309c0fc3f142246c5be2c6feaf5313ca1f4
Omkar Jagtap <omkarjagtap1011@gmail.com>
2024-05-24 17:35:28 +0530

    Testing

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -60,11 +60,15 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 // });
 
 const createBookingCheckout = async (session) => {
-  const tour = session.client_reference_id;
-  const user = (await User.findOne({ email: session.customer_details.email }))
-    .id;
-  const price = session.object.amount_total / 100;
-  await Booking.create({ tour, user, price });
+  try {
+    const tour = session.client_reference_id;
+    const user = (await User.findOne({ email: session.customer_details.email }))
+      .id;
+    const price = session.object.amount_total / 100;
+    await Booking.create({ tour, user, price });
+  } catch (err) {
+    console.log(session);
+  }
 };
 
 exports.webhookCheckout = (req, res, next) => {
