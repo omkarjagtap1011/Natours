1c730a78f8259ba62183aa748e997dfe2afe2957
Omkar Jagtap <omkarjagtap1011@gmail.com>
2024-05-24 17:16:32 +0530

    Solved Stripe Error

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -14,7 +14,9 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   const product = await stripe.products.create({
     name: `${tour.name} Tour`,
     description: tour.summary,
-    images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+    images: [
+      `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`,
+    ],
   });
 
   const price = await stripe.prices.create({
@@ -60,7 +62,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].price.unit_amount / 100;
+  const price = session.display_items[0].price.unit_amount / 100;
   await Booking.create({ tour, user, price });
 };
 
