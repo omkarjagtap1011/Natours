2757184cfa3ee54146caf622dc1d8be84ffa0693
Omkar Jagtap <omkarjagtap1011@gmail.com>
2024-05-24 16:01:07 +0530

    Checking another error

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
-const DB = process.env.Database.replace(
+const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD,
 );
